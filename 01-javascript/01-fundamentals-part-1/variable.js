1dbedfcca5079b285d139bee70368f8718e91a90
satyam kumar <satyamkumar@satyams-MacBook-Air.local>
2026-02-04 14:13:02 +0530

    JS: fundamentals part 1 – variables and naming conventions

---
+++ 01-javascript/01-fundamentals-part-1/variable.js
@@ -0,0 +1,21 @@
+console.log('satyam');
+
+let js = 'amazing';
+console.log(js);
+
+let firstName = 'Satyam';
+let lastName = 'Kumar';
+console.log(firstName, lastName);
+
+// variables should not start with numbers and vaiables should only contain dollar and underscore,
+// variable should not have space in between,
+// variables which never gona change can be used in upper case
+// we must use camelCase for naming variables,
+
+let first$Name = 'Sunny';
+let last_name = 'Singh';
+
+let age43 = 28;
+let RED = '#ff0000';
+
+console.log(first$Name, last_name, age43, RED);
